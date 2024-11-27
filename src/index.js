91caa1ecba626782c7aef96e89a3c4b6a70e9a1a
Ayaana62 <ayaanaboakye44@gmail.com>
2024-11-27 15:07:13 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,33 @@
+let name = "Ayaana";
+console.log(name);
+
+let age = 19;
+console.log(age);
+
+let food = 9+8;
+console.log(food);
+
+let girls = 10-5;
+console.log(girls);
+
+let boys = 16/18
+console.log(boys);
+
+let fathers = 6*7
+console.log(fathers);
+
+let mothers = 5%4
+console.log (mothers);
+
+let Banku = true
+console.log (Banku);
+
+let shito = 10
+console.log ("number");
+
+let onemanthousand = ("everybody");
+console.log  ("string");
+
+
+
+
